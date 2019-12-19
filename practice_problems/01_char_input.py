b3d2cf40c14bce64b22fb991f6fe1eaf82c6f598
Daniel Parham <dparha012@gmail.com>
2019-12-19 12:31:46 -0600

    initial commit

---
+++ practice_problems/01_char_input.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# take input and print it the amount of times dictated
+
+user = input("Enter a string: ")
+num = int(input("Enter an integer: "))
+
+print(num * (user + " "))
+
