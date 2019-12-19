89d84cdd9df49ac4aa7b4b4f45c8a5773f44fcff
Daniel Parham <dparha012@gmail.com>
2019-12-19 16:32:02 -0600

    added list functionality

--- practice_problems/03_list_less_than_5.py
+++ practice_problems/03_list_less_than_5.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
-# prints numbers in list less than 5
+# prints a list numbers in list less than 5
 
 list = list(input("enter a list of integers: \n"))
+new_list = []
 
 for i in list:
     if i.isdigit() and int(i) < 5:
-        print(i)
+        new_list.append(i)
     elif not i.isdigit():
         print(i + " is not an integer")
+
+if not new_list:
+    print("No numbers less than 5")
+else:
+    print(new_list)
