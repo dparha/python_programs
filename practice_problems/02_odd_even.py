988338823b0106e5e07b6b109a23643b10de954d
Daniel Parham <dparha012@gmail.com>
2019-12-19 13:10:52 -0600

    added while loop for incorrect input

--- practice_problems/02_odd_even.py
+++ practice_problems/02_odd_even.py
@@ -2,29 +2,37 @@
 # determines if a number is odd or even or
 # checks if a number divides evenly into another number
 
+run = 1
 
-option = int(input("enter 1 or 2 "))
+while run == 1:
+    option = int(input("enter 1 or 2 "))
 
-if option == 1:
+    if option == 1:
 
-    num = int(input("enter a number: "))
+        num = int(input("enter a number: "))
 
-    if num % 2 and num % 4:
-        print("odd")
+        if num % 2 and num % 4:
+            print("odd")
+            break
 
-    elif not num % 2 and not num % 4:
-        print("even and divisible by 4")
+        elif not num % 2 and not num % 4:
+            print("even and divisible by 4")
+            break
+
+        else:
+            print("even but not divisible by 4")
+            break
+
+    elif option == 2:
+        num = int(input("enter first number: "))
+        check = int(input("enter second number: "))
+
+        if num % check:
+            print(str(check) + " does not divide evenly into " + str(num))
+            break
+        else:
+            print(str(check) + " divides evenly into " + str(num))
+            break
 
     else:
-        print("even but not divisible by 4")
-
-elif option == 2:
-    num = int(input("enter first number: "))
-    check = int(input("enter second number: "))
-
-    if num % check:
-        print(str(check) + " does not divide evenly into " + str(num))
-    else:
-        print(str(check) + " divides evenly into " + str(num))
-else:
-    print("must enter 1 or 2")
+        print("must enter 1 or 2\n")
