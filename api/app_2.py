9c9b1016141076c5998c79935296d23c711dc020
Daniel Parham <dparha012@gmail.com>
2020-01-12 09:58:55 -0600

    more app practice

---
+++ api/app_2.py
@@ -0,0 +1,25 @@
+
+from flask import Flask, request, jsonify
+app = Flask(__name__)  # Flask constructor
+
+# A decorator used to tell the app
+# which URL is associated function
+
+
+@app.route('/')
+def hello():
+    return 'HELLO'
+
+
+@app.route('/hello/<string:name>', methods=['GET', 'POST'])
+def hello_name(name):
+    if request.method == 'GET':
+        return 'Hello %s!\n' % name
+
+    if request.method == 'POST':
+        return jsonify({'name from post': request.form['name'],
+                        'name from url': name})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
