89de6f15942565c6d18fd7cf783684d631cfd7b7
Daniel Parham <dparha012@gmail.com>
2020-01-11 22:14:14 -0600

    flask hello world api

---
+++ api/app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# Using flask to make an api
+from flask import Flask, jsonify, request
+
+# creating a Flask app
+app = Flask(__name__)
+
+# on the terminal type: curl http://127.0.0.1:5000
+# returns hello world when we use GET.
+# returns the data that we send when we use POST.
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if(request.method) == 'GET':
+
+        data = "hello world"
+        return jsonify({'data': data})
+
+# A simple funciton to calculate the square of a number
+# the number to squared is sent in the URL when we use GET
+# on the terminal type: curl http://127.0.0.1:5000/home/10
+# this return 100 (square of 10)
+
+
+@app.route('/home/<int:num>', methods=['GET'])
+def disp(num):
+
+    return jsonify({'data': num ** 2})
+
+# driver function
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
